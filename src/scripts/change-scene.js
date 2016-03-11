107ad30d964e5a7686db62bb0bef47ed5a3db3d2
Alex Bezuska <abezuska@wild-aster.local>
2016-03-11 15:25:17 -0500

    replace code for play button

--- src/scripts/change-scene.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
-  var scene = game.entities.get(entity, "change-scene");
-  game.switchScene(scene);
-};
--- src/systems/simulation/scene-change-button.js
+++ src/systems/simulation/scene-change-button.js
@@ -4,18 +4,16 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
   game.entities.registerSearch("scene-change-button-search", ["change-scene", "position", "size"]);
   ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
 
-    var timers = game.entities.get(entity, "timers");
-    var image = game.entities.get(entity, "image");
-    var pressedImage = game.entities.get(entity, "pressed-image");
-
-    if (game.inputs.buttonPressed("action") && !timers.pressed.running) {
-
-      timers.pressed.running = true;
-      game.sounds.play("ScoreUp.mp3");
+    if (game.inputs.button("action")) {
+      var image = game.entities.get(entity, "image");
+      var pressedImage = game.entities.get(entity, "pressed-image");
+      image.name = pressedImage.name;
     }
 
-    if (timers.pressed.running) {
-      image.name = pressedImage.name;
+    if (game.inputs.buttonReleased("action")) {
+      var scene = game.entities.get(entity, "change-scene");
+      game.switchScene(scene);
+      game.sounds.play("ScoreUp.mp3");
     }
 
   }, "scene-change-button-search");
