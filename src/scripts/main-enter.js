4ff91f8a46283e3596917b2826b10eb4cc06fbb7
Eric Lathrop <eric@ericlathrop.com>
2016-03-10 22:41:19 -0600

    Generate grid

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,6 +1,25 @@
 "use strict";
 
+function makeTile(game, x, y) {
+  var tile = game.instantiatePrefab("symbolCircle");
+  var position = game.entities.get(tile, "position");
+  position.x = x;
+  position.y = y;
+}
+
+var gridWidth = 7;
+var gridHeight = 7;
+var tileSize = 123;
+var tilePadding = 26;
+var gridPadding = 32;
+
 module.exports = function(game) { // eslint-disable-line no-unused-vars
   game.scaleCanvasToFitRectangle(1080, 1920);
   game.sounds.play("ThunderLoop.mp3", true);
+  var gridPos = game.entities.get(2, "position");
+  for (var y = 0; y < gridHeight; y++) {
+    for (var x = 0; x < gridWidth; x++) {
+      makeTile(game, gridPos.x + gridPadding + (x * (tileSize + tilePadding)), gridPos.y + gridPadding + (y * (tileSize + tilePadding)));
+    }
+  }
 };
