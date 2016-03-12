f2e4b47b873a8d8f8a6483db1d49f8cde06f8e03
Alex Bezuska <abezuska@gmail.com>
2016-03-12 08:58:41 -0500

    add logo and lightning sounds

--- src/systems/simulation/trigger-lightning.js
+++ src/systems/simulation/trigger-lightning.js
@@ -48,10 +48,28 @@ function spawnLightning(x1, y1, x2, y2, game, forkChance) {
   }
 }
 
+function randomFrom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var lightningSounds = [
+  "lightning1.wav",
+  "lightning2.wav",
+  "lightning3.wav",
+  "lightning4.wav",
+  "lightning5.wav",
+  "lightning6.wav",
+  "lightning7.wav",
+  "lightning8.wav",
+  "lightning9.wav",
+  "lightning10.wav",
+]
+
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
   ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
     var input = game.entities.get(entity, "triggerLightning").input;
     if (game.inputs.buttonPressed(input)) {
+      game.sounds.play(randomFrom(lightningSounds));
       var mouse = getMousePos(game);
       spawnLightning(0, 0, mouse.x, mouse.y, game, 0.7);
       spawnLightning(1080, 0, mouse.x, mouse.y, game, 0.7);
