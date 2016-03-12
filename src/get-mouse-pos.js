de081ae55cd76ff4ff632a83c6a101f896f51057
Eric Lathrop <eric@ericlathrop.com>
2016-03-11 22:15:31 -0700

    Extract getMousePos

---
+++ src/get-mouse-pos.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = function(game) {
+  var camera = game.entities.find("camera")[0];
+  var cameraPosition = game.entities.get(camera, "position");
+
+  var mx = game.inputs.mouse.x + cameraPosition.x;
+  var my = game.inputs.mouse.y + cameraPosition.y;
+
+  return {
+    x: mx,
+    y: my
+  };
+};
--- src/systems/simulation/drag-position-y.js
+++ src/systems/simulation/drag-position-y.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var getMousePos = require("../../get-mouse-pos");
+
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
   game.entities.registerSearch("drag-position-y", ["dragY", "position", "size"]);
   ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
@@ -7,44 +9,40 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
       return;
     }
 
-    var camera = game.entities.find("camera")[0];
-    var cameraPosition = game.entities.get(camera, "position");
-
     var position = game.entities.get(entity, "position");
     var size = game.entities.get(entity, "size");
-    var mx = game.inputs.mouse.x + cameraPosition.x;
-    var my = game.inputs.mouse.y + cameraPosition.y;
+    var mouse = getMousePos(game);
     var drag = game.entities.get(entity, "dragY");
 
-    var inside = mx >= position.x
-        && mx < position.x + size.width
-        && my >= position.y
-        && my < position.y + size.height;
+    var inside = mouse.x >= position.x
+        && mouse.x < position.x + size.width
+        && mouse.y >= position.y
+        && mouse.y < position.y + size.height;
 
     // FIXME: this is bullshit
     var gridPosition = game.entities.get(2, "position");
     var gridSize = game.entities.get(2, "size");
-    var insideGrid = mx >= gridPosition.x
-      && mx < gridPosition.x + gridSize.width
-      && my >= gridPosition.y
-      && my < gridPosition.y + gridSize.height;
+    var insideGrid = mouse.x >= gridPosition.x
+      && mouse.x < gridPosition.x + gridSize.width
+      && mouse.y >= gridPosition.y
+      && mouse.y < gridPosition.y + gridSize.height;
 
     if (game.inputs.buttonPressed("action") && inside) {
       drag.startX = position.x;
       drag.startY = position.y;
-      drag.offsetX = position.x - mx;
-      drag.offsetY = position.y - my;
+      drag.offsetX = position.x - mouse.x;
+      drag.offsetY = position.y - mouse.y;
     } else if (game.inputs.button("action") && drag.offsetX !== undefined && drag.offsetY !== undefined && insideGrid) {
-      var dx = Math.abs(mx + drag.offsetX - drag.startX);
-      var dy = Math.abs(my + drag.offsetY - drag.startY);
+      var dx = Math.abs(mouse.x + drag.offsetX - drag.startX);
+      var dy = Math.abs(mouse.y + drag.offsetY - drag.startY);
       if (dx > dy && dy > 30) {
         position.x = drag.startX;
         position.y = drag.startY;
         delete drag.offsetX;
         delete drag.offsetY;
       } else {
-        // position.x = mx + drag.offsetX;
-        position.y = my + drag.offsetY;
+        // position.x = mouse.x + drag.offsetX;
+        position.y = mouse.y + drag.offsetY;
       }
     } else if (game.inputs.buttonReleased("action") && drag.offsetX !== undefined && drag.offsetY !== undefined) {
       position.x = drag.startX;
