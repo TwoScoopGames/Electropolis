d652fa81326f40e88a34bb481e574d663045cf1f
Rex Soriano <awesomerex@gmail.com>
2016-03-12 14:07:10 -0800

    adjust decrement speed and add more clouds

--- src/init-clouds.js
+++ src/init-clouds.js
@@ -4,24 +4,23 @@ var init = function(game) {
   var camera = game.entities.get(0, "position");
   var clouds = ["bg-cloud1", "bg-cloud2", "bg-cloud3"];
 
-  for (var i = 0; i < 9; i++) {
+  for (var i = 0; i < 12; i++) {
     var cloud1 = game.instantiatePrefab(clouds[Math.floor(Math.random() * clouds.length)]);
     var cloud1Position = game.entities.get(cloud1, "position");
     var cloud1Velocity = game.entities.get(cloud1, "velocity");
 
-    if (i < 3) {
-      console.log(i, i % 3, (camera.x + (500 * (i % 3))));
-      cloud1Position.x = (camera.x + (600 * (i % 3)));
+    if (i < 4) {
+      cloud1Position.x = (camera.x + (600 * (i % 4)));
       cloud1Position.y = 0;
       cloud1Velocity.x = -.0575;
     }
-    if (i >= 3 && i < 6) {
-      cloud1Position.x = (camera.x + (600 * (i % 3)));
+    if (i >= 4 && i < 8) {
+      cloud1Position.x = (camera.x + (600 * (i % 4)));
       cloud1Position.y = 400;
       cloud1Velocity.x = -.06;
     }
-    if (i >= 6 && i < 9) {
-      cloud1Position.x = (camera.x + (600 * (i % 3)));
+    if (i >= 8 && i < 12) {
+      cloud1Position.x = (camera.x + (600 * (i % 4)));
       cloud1Position.y = 800;
       cloud1Velocity.x = -.09;
     }
